fd3040a65408940baafe4b79ba3c305d56b85355
Huizerd <jessehagenaars@gmail.com>
2018-12-10 09:58:23 +0100

    Assigning to prdataset now works properly

--- main.m
+++ main.m
@@ -15,7 +15,7 @@ addpath(genpath(fileparts(which(mfilename))))
 %% Load data
 
 % Show some data, this will load digits 0-9 with 25 examples each
-sample = prnist(0:9, 1:100:1000);
+sample = prnist(0:9, 1:40:1000);
 figure('Name', 'Sample')
 show(sample)
 
@@ -23,6 +23,9 @@ show(sample)
 % data = prnist(0:9, 1:1000);
 
 %% Preprocess data
+
+% TODO: do we want to resize twice or only after deslanting?
+% TODO: do we want to binarize again after deslanting?
 
 % Make squares
 squared = im_box(sample, [5 5 5 5], 1);
@@ -42,12 +45,11 @@ dummy = zeros(size(preprocessed));
 for i = 1:size(resized, 1)
     clean = remove_noise(data2im(resized(i)));
     straight = deslant(clean);
-%     dummy(1, :) = reshape(im_resize(straight, [50 50]), 1, size(dummy, 2));
-    dummy(1, :) = reshape(clean, 1, size(dummy, 2));
+    dummy(i, :) = reshape(im_resize(straight, [50 50]), 1, size(dummy, 2));
 end
 
 % Assign to prdataset
-setdata(preprocessed, dummy)
+preprocessed = setdata(preprocessed, dummy);
 
 figure('Name', 'Clean')
 show(preprocessed)
