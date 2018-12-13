7ca5f3d9f95c1215c44ad464bd73d6607948550b
Ilmari Vikström <ilmarimikaelvikstrom@gmail.com>
2018-12-13 18:26:06 +0100

    Implemented HOG feature extraction. Tried multiple classifiers. Fisherc and bpxnc seem to work quite alright. Accuracy between 4-20%.

--- main.m
+++ main.m
@@ -53,3 +53,74 @@ preprocessed = setdata(preprocessed, dummy);
 
 figure('Name', 'Clean')
 show(preprocessed)
+
+
+%% HOG feature classification
+
+%img = reshape(preprocessed.data(1,:), 50, 50);
+%[hog_2x2, vis2x2] = extractHOGFeatures(img,'CellSize',[2 2]);
+%[hog_3x3, vis3x3] = extractHOGFeatures(img,'CellSize',[3 3]);
+%[hog_4x4, vis4x4] = extractHOGFeatures(img,'CellSize',[4 4]);
+%[hog_8x8, vis8x8] = extractHOGFeatures(img,'CellSize',[8 8]);
+%[hog_16x16, vis16x16] = extractHOGFeatures(img,'CellSize',[16 16]);
+%[hog_25x25, vis25x25] = extractHOGFeatures(img,'CellSize',[25 25]);
+
+
+%hogFeatureSize = 36; cellSize = [25 25];
+%hogFeatureSize = 144; cellSize = [16 16];
+hogFeatureSize = 900; cellSize = [8 8];
+%hogFeatureSize = 4356; cellSize = [4 4];
+%hogFeatureSize = 8100; cellSize = [3 3];
+%hogFeatureSize = 20736; cellSize = [2 2];
+
+[trainingSet, testSet, trainingIndices, testIndices] = gendat(preprocessed, 0.7);
+
+numTrainingImages = size(trainingSet,1);
+trainingFeatures = zeros(numTrainingImages, hogFeatureSize, 'single');
+for i = 1:numTrainingImages
+    trainingFeatures(i, :) = extractHOGFeatures(reshape(trainingSet.data(i,:), 50, 50), 'CellSize', cellSize);  
+end
+trainingLabels = trainingSet.nlab;
+trainingFeatures = double(trainingFeatures);
+trainingFeatures = prdataset(trainingFeatures, trainingLabels);
+
+numTestImages = size(testSet,1);
+testFeatures = zeros(numTestImages, hogFeatureSize, 'single');
+for i = 1:numTestImages
+    testFeatures(i, :) = extractHOGFeatures(reshape(testSet.data(i,:), 50, 50), 'CellSize', cellSize);
+end
+testLabels = testSet.nlab;
+testFeatures = double(testFeatures);
+testFeatures = prdataset(testFeatures, testLabels);
+
+
+%% Train a classifier
+classifier = bpxnc(trainingFeatures, [100 20], 10000);
+
+
+%% Evaluate classifier
+clfdLabs = testFeatures * classifier * labeld;
+cm = confmat(testLabels, clfdLabs)
+testc(testFeatures, classifier)
+
+
+%% Cross-validate
+allFeatures = [trainingFeatures; testFeatures];
+error = prcrossval(allFeatures, vpc, 5, 1)
+
+
+%% Visualize the HOG features
+figure; 
+subplot(2,3,1:3); imshow(img);
+
+subplot(2,3,4);  
+plot(vis2x2); 
+title({'CellSize = [2 2]'; ['Length = ' num2str(length(hog_2x2))]});
+
+subplot(2,3,5);
+plot(vis4x4); 
+title({'CellSize = [4 4]'; ['Length = ' num2str(length(hog_4x4))]});
+
+subplot(2,3,6);
+plot(vis8x8); 
+title({'CellSize = [8 8]'; ['Length = ' num2str(length(hog_8x8))]});
