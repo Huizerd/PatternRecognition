25806ccb9f3ae7345b5d75cfe6341c84238ff0d9
Huizerd <jessehagenaars@gmail.com>
2018-12-25 22:16:11 +0100

    Did live digits

--- main.m
+++ main.m
@@ -36,17 +36,13 @@ squared = im_box(raw_data, [5 5 5 5], 1);
 
 % We want to end up with images of 50x50 pixels
 image_size = [50 50];
-data = zeros(size(squared, 1), prod(image_size));
 
 % Denoise and remove slant, then resize
-for i = 1:size(squared, 1)
-    clean = remove_noise(data2im(squared(i)));
-    straight = deslant(clean);
-    data(i, :) = reshape(im_resize(straight, image_size), 1, prod(image_size));
-end
+prep_map = squared * filtim('remove_noise') * filtim('deslant') * im_resize(image_size);
 
-% Create prdataset
-preprocessed = prdataset(data, squared.labels);
+% Put in empty prdataset
+prep_map = prdataset(prep_map);
+preprocessed = prdataset(prep_map.data, prep_map.labels);
 preprocessed = setfeatsize(preprocessed, image_size);
 preprocessed = setname(preprocessed, 'preprocessed NIST');
 
@@ -59,7 +55,7 @@ preprocessed = setname(preprocessed, 'preprocessed NIST');
 % TODO: try PCA & Fisher together
 
 % Do PCA
-eigendigits = 50; % first 50 eigendigits
+eigendigits = 100; % first 50 eigendigits
 pca = pcam(preprocessed, eigendigits);
 % figure
 % show(pca)
@@ -82,21 +78,13 @@ features_pca = setfeatsize(features_pca, [1 eigendigits]);
 features_pca = setprior(features_pca, getprior(features_pca)); % avoid warnings
 features_pca = setname(features_pca, 'features PCA');
 
-% TODO: incorporate feature extraction into loop --> my_rep
-
-% Set parameters and create dummy matrix for HOG
+% Set parameters for HOG
 cell_size = [4 4];
-hog_size = size(extractHOGFeatures(reshape(preprocessed.data(1, :), preprocessed.featsize), 'CellSize', cell_size), 2);
-data = zeros(size(preprocessed, 1), hog_size);
 
 % Extract HOG features per image
-for i = 1:size(preprocessed, 1)
-    data(i, :) = extractHOGFeatures(reshape(preprocessed.data(i, :), preprocessed.featsize), 'CellSize', cell_size);
-end
+features_hog = preprocessed * filtim('extractHOGFeatures', {'CellSize', cell_size});
 
-% Create HOG prdataset
-features_hog = setdata(preprocessed, data);
-features_hog = setfeatsize(features_hog, [1 hog_size]);
+% Adjust HOG prdataset
 features_hog = setprior(features_hog, getprior(features_hog)); % avoid warnings
 features_hog = setname(features_hog, 'features HOG');
 
@@ -119,21 +107,23 @@ rng('default')
 
 % Test various classifiers
 features = {features_pca, features_hog};
-classifiers = {svc, knnc, loglc};
+classifiers = {svc, knnc, randomforestc};
 [error, ~, labels_out] = prcrossval(features, classifiers, 5, 1);
 disp(error)
 
 % Do classifier evaluation for various training set sizes
-% train_sizes = [10 50 100 200 500];
-% error_train_size = cleval(features, classifiers, train_sizes, 10);
+% train_sizes = [10 100 200 500 800];
+% error_train_size = cleval(features, classifiers, train_sizes, 5);
 % figure
 % plote(error_train_size)
 
 %%
 
+% classifiers = {svc, knnc, randomforestc};
+
 % Do classifier evaluation for various feature set sizes
-feat_size_pca = [1 5 10 20 50];
-train_test_split = 0.7;
-error_feat_size_pca = clevalf(features_pca, classifiers, feat_size_pca, train_test_split, 10);
-figure
-plote(error_feat_size_pca)
+% feat_size_pca = [1 5 10 50 80 100];
+% train_test_split = 0.7;
+% error_feat_size_pca = clevalf(features_pca, classifiers, feat_size_pca, train_test_split, 5);
+% figure
+% plote(error_feat_size_pca)
