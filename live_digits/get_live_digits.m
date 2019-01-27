41740ab32977e058b159da3b6e891eeb44d51e97
Huizerd <jessehagenaars@gmail.com>
2019-01-27 11:30:55 +0100

    Train size for live digits

--- live_digits/get_live_digits.m
+++ live_digits/get_live_digits.m
@@ -1,10 +1,12 @@
-function live_digits = get_live_digits(scan_dir, image_size)
+function live_digits = get_live_digits(scan_dir, image_size, n_digits)
 % GET_LIVE_DIGITS Processes scanned images containing handwritten digits.
 %
 % Inputs:
 % - scan_dir: location containing scanned image(s) of several handwritten
 %   digits
 % - image_size: desired size of output images
+% - n_digits: number of digits per scanned image, set false for answering
+%   manually
 %
 % Outputs:
 % - live_digits: prdataset of separate binarized digits
@@ -15,8 +17,14 @@ function live_digits = get_live_digits(scan_dir, image_size)
 scan_files = dir([scan_dir '*.jpg']);
 for i = 1:size(scan_files, 1)
     
-    % Get images and labels
+    % Get images, convert to grayscale if needed
     scans{i, 1} = imread([scan_dir scan_files(i).name]);
+    
+    if size(scans{i, 1}, 3) > 1
+        scans{i, 1} = rgb2gray(scans{i, 1});
+    end
+    
+    % Get labels
     label = strsplit(scan_files(i).name, '.'); % take off .jpg
     
     % Show for clarity
@@ -24,13 +32,16 @@ for i = 1:size(scan_files, 1)
     title(label{1})
     
     % Get info about what's in the image
-    scans{i, 2} = input('Does the image contain one digit class, is it correctly oriented (i.e. upright) and is the label correct (e.g. digit_0.jpg)? [yes/no] ', 's');
-    scans{i, 3} = label{1};
-    scans{i, 4} = input('Number of objects in this image: ');
+    if ~n_digits
+        scans{i, 2} = input('Does the image contain one digit class, is it correctly oriented (i.e. upright) and is the label correct (e.g. digit_0.jpg)? [yes/no] ', 's');
+        scans{i, 3} = label{1};
+        scans{i, 4} = input('Number of objects in this image: ');
+    else
+        scans{i, 2} = 'yes';
+        scans{i, 3} = label{1};
+        scans{i, 4} = n_digits(i);
+    end
 end
-
-% Close all image windows
-close all
 
 % Now process
 for i = 1:size(scans, 1)
--- main.m
+++ main.m
@@ -14,13 +14,10 @@ prmemory(100e6)
 % Add subfolders to path
 addpath(genpath(fileparts(which(mfilename))))
 
-% Add subfolders to path
-addpath(genpath(fileparts(which(mfilename))))
-
 %% Load data
 
-% Load digits 0-9 with 25 examples each
-raw_data = prnist(0:9, 1:100:1000);
+% Load digits 0-9 with 500 examples each
+raw_data = prnist(0:9, 1:2:1000);
 
 % Show data
 % figure('Name', 'Data')
@@ -55,26 +52,27 @@ features_hog = get_hog(preprocessed, cell_size);
 
 % Combination of HOG and PCA, larger cell size to allow PCA to capture more
 %   global features instead of noise
-features_hog_large = get_hog(preprocessed, [8 8]);
+large_cell_size = [8 8];
+features_hog_large = get_hog(preprocessed, large_cell_size);
 hog_size = [1 size(features_hog_large, 2)];
 [u_hog_pca, ~] = get_pca(features_hog_large, components, hog_size);
 
 %% Cross-validation
 
 % Test various classifiers: k-NN and SVC need scaling!
-classifiers_pca = {fisherc, knnc, libsvc}; % since PCA already has scaling
+classifiers_pca = {fisherc, knnc, svc, libsvc}; % since PCA already has scaling
 classifiers_hog = {fisherc, scalem('variance') * knnc, ...
-    scalem('variance') * libsvc};
+    scalem('variance') * svc, scalem('variance') * libsvc};
 
-[error_pca, ~, ~] = prcrossval(preprocessed, u_pca * classifiers_pca, ...
-    5, 1);
-[error_hog, ~, ~] = prcrossval(features_hog, classifiers_hog, 5, 1);
-[error_hog_pca, ~, ~] = prcrossval(features_hog_large, ...
-    u_hog_pca * classifiers_pca, 5, 1);
-
-disp(error_pca)
-disp(error_hog)
-disp(error_hog_pca)
+% [error_pca, ~, ~] = prcrossval(preprocessed, u_pca * classifiers_pca, ...
+%     5, 1);
+% [error_hog, ~, ~] = prcrossval(features_hog, classifiers_hog, 5, 1);
+% [error_hog_pca, ~, ~] = prcrossval(features_hog_large, ...
+%     u_hog_pca * classifiers_pca, 5, 1);
+% 
+% disp(error_pca)
+% disp(error_hog)
+% disp(error_hog_pca)
 
 % Do classifier evaluation for various training set sizes
 % train_sizes = [5 10 200 300];
@@ -131,21 +129,93 @@ disp(error_hog_pca)
 % features_hog = get_hog(preprocessed, cell_size);
 % with the best cell_size!!!
 
+%% Training set size
+
+% Experiment with these training set sizes, remaining samples are used for
+%   testing!
+train_sizes = [5 10 50 100 200];
+
+% Fisher
+features_fisher = get_hog(preprocessed, [8 8]);
+comp_fisher = [70 90];
+for i = 1:length(comp_fisher)
+    if comp_fisher(i) ~= 0
+        [clf_fisher{1, i}, ~] = get_pca(features_fisher, ...
+            comp_fisher(i), [1 size(features_fisher, 2)]);
+    else
+        clf_fisher{1, i} = 1;
+    end
+end
+
+e_train_fisher = cleval(features_fisher, clf_fisher * fisherc, ...
+    train_sizes, 5);
+
+figure('Name', 'Error train: Fisher')
+plote(e_train_fisher)
+set(gca, 'XScale', 'linear')
+
+% k-NN
+features_knn = get_hog(preprocessed, [8 8]);
+[pca_knn, ~] = get_pca(features_knn, 70, [1 size(features_knn, 2)]);
+e_train_knn = cleval(features_knn, pca_knn * knnc, train_sizes, 5);
+
+figure('Name', 'Error train: k-NN')
+plote(e_train_knn)
+set(gca, 'XScale', 'linear')
+
+% SVC (svc)
+features_svc = get_hog(preprocessed, [8 8]);
+e_train_svc = cleval(features_svc, scalem('variance') * svc, ...
+    train_sizes, 5);
+
+figure('Name', 'Error train: SVC (svc)')
+plote(e_train_svc)
+set(gca, 'XScale', 'linear')
+
+% SVC (libsvc)
+features_libsvc = get_hog(preprocessed, [8 8]);
+[pca_libsvc, ~] = get_pca(features_libsvc, 60, ...
+    [1 size(features_libsvc, 2)]);
+e_train_libsvc = cleval(features_libsvc, pca_libsvc * libsvc, ...
+    train_sizes, 5);
+
+figure('Name', 'Error train: SVC (libsvc)')
+plote(e_train_libsvc)
+set(gca, 'XScale', 'linear')
+
+% Create master plot
+e_train_all = e_train_fisher;
+for s = [e_train_knn e_train_svc e_train_libsvc]
+    for f = {'error', 'std', 'apperror', 'appstd', 'names'}
+        
+        if ~strcmp(f{1}, 'names')
+            e_train_all.(f{1}) = vertcat(e_train_all.(f{1}), s.(f{1}));
+        else
+            e_train_all.(f{1}) = char(vertcat(cellstr(e_train_all.(f{1})), ...
+                cellstr(s.(f{1}))));
+        end
+    end
+end
+
+figure('Name', 'Error train: all')
+plote(e_train_all)
+set(gca, 'XScale', 'linear')
+
 %% Benchmark
 
 % Train SVM
-classifier_hog = libsvc(features_hog);
-classifier_pca = preprocessed * (u_pca * libsvc);
-classifier_hog_pca = features_hog_large * (u_hog_pca * libsvc);
+% classifier_hog = libsvc(features_hog);
+% classifier_pca = preprocessed * (u_pca * libsvc);
+% classifier_hog_pca = features_hog_large * (u_hog_pca * libsvc);
 
-bench_error_hog = nist_eval('hog_rep', classifier_hog, 100);
-disp(bench_error_hog)
-
-bench_error_pca = nist_eval('pca_rep', classifier_pca, 100);
-disp(bench_error_pca)
-
-bench_error_hog_pca = nist_eval('combined_rep', classifier_hog_pca, 100);
-disp(bench_error_hog_pca)
+% bench_error_hog = nist_eval('hog_rep', classifier_hog, 100);
+% disp(bench_error_hog)
+% 
+% bench_error_pca = nist_eval('pca_rep', classifier_pca, 100);
+% disp(bench_error_pca)
+% 
+% bench_error_hog_pca = nist_eval('combined_rep', classifier_hog_pca, 100);
+% disp(bench_error_hog_pca)
 
 % Get datasets
 
@@ -153,6 +223,52 @@ disp(bench_error_hog_pca)
 
 % Scenario 2
 
+%% Live classification
+
+% Train on 200
+raw_data = prnist(0:9, 1:5:1000);
+preprocessed = preprocessing(raw_data, [50 50], [5 5 5 5], true);
+features = get_hog(preprocessed, [8 8]);
+
+% Classifiers
+% [pca_fisher, ~] = get_pca(features, 90, [1 size(features, 2)]);
+[pca_libsvc, ~] = get_pca(features, 60, [1 size(features, 2)]);
+% classifier_fisher = features * (pca_fisher * fisherc);
+% classifier_svc = features * (scalem('variance') * svc);
+classifier_libsvc = features * (pca_libsvc * libsvc);
+
+% Hard-coded answers to questions the script will ask
+% n_digits = [75 77 80 77 83 85 80 89 90 88];
+
+% Get scanned digits
+live_data = get_live_digits('data/', [50 50], n_digits);
+
+% Show how ugly my writing is
+figure('Name', 'Live digits')
+show(live_data)
+
+% Feature extraction
+features_live = get_hog(live_data, [8 8]);
+
+% Classify
+% error_live = features_live * ...
+%     {classifier_fisher, classifier_svc, classifier_libsvc} * testc;
+% disp(error_live)
+
+% Get confusion matrix for best
+confmat(features_live * classifier_libsvc)
+% confmat(features_live * classifier_svc)
+
+% Do classifier evaluation for various training set sizes
+% train_sizes = [5 10 50 100 200];
+% untrained_clf = {pca_fisher * fisherc, scalem('variance') * svc, ...
+%     pca_libsvc * libsvc};
+% e_train_size_live = cleval(features, untrained_clf, train_sizes, 5, ...
+%     features_live);
+% 
+% figure('Name', 'Error train size live digits')
+% plote(e_train_size_live)
+% set(gca, 'XScale', 'linear')
 
 
 
